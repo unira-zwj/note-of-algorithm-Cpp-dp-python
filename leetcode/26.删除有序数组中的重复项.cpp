b1a2f91cc979029569eba76005df4a43433ab64a
dcjyw9934 <1279404304@qq.com>
2021-06-21 14:21:40 +0800

    26

--- leetcode/26.删除有序数组中的重复项.cpp
+++ leetcode/26.删除有序数组中的重复项.cpp
@@ -4,19 +4,30 @@
  * [26] 删除有序数组中的重复项
  */
 
+/*
+ * 这一题一定要注意return的值，只能返回整数型return a。
+ */
+
+
 // @lc code=start
+#include<vector>
+
+using namespace std;
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int a = 0;
         for ( int i = 0; i < nums.size(); i++){
-            if (i == 0 || nums[a-1] != nums[i]){
+            if (i == 0 || nums[i -1] != nums[i]){
                 nums[a] = nums[i];
                 a++;
             }
         }
-        return nums.size();
+        return a;
     }
 };
 // @lc code=end
+
+
 
